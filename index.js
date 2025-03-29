f6dbceafd718788d89fe3b16b08a0d99ca2c4b05
walls38 <walls38@purdue.edu>
2025-03-29 18:51:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const selectedProfile = ()=> {
+    $("#name").text("Annie Student");
+    $("#major").text("CS");
+    $("#year").text("2025");
+    $("h4").text("USA")
+};
+
+$(document).ready(selectedProfile)
